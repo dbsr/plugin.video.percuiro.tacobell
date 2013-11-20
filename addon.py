be8a3ce999c41b727c24fc8cf7cc0b6fcb17be57
Daan <daanmathot@gmail.com>
2013-11-20 02:06:18 +0100

    8

--- addon.py
+++ addon.py
@@ -65,9 +65,12 @@ def list_results(results):
                 path = plugin.url_for('provider_resolve', url=result['url'], provider=provider)
             items.append(dict(
                 label=result['label'],
-                path=path
+                path=path,
+                is_playable=True
             ))
     return items
+
+uu
 
 
 @plugin.route('/resolve/<provider>/<url>', name='provider_resolve')
