f2cd28657a3cc17257e9fec4a16875c0168fa065
Daan <daanmathot@gmail.com>
2013-12-17 08:54:14 +0100

    util scripts

--- resources/percuiro_lib/percuiro/__init__.py
+++ resources/percuiro_lib/percuiro/__init__.py
@@ -13,12 +13,20 @@ def get_providers():
                 for provider in providers)
 
 
-def get_plugin_providers(plugin):
-    return sorted(
+def get_plugin_providers(plugin, include_disabled=False):
+    print include_disabled
+    plugin_providers = sorted(
         map(
             lambda provider: PluginProvider(plugin, **provider),
             providers),
         key=lambda provider: provider.priority)
+    if include_disabled:
+        return plugin_providers
+    for provider in plugin_providers:
+        print provider, provider.status
+    return filter(
+        lambda provider: provider.status == 'enabled',
+        plugin_providers)
 
 
 def get_plugin_provider(plugin, provider):
